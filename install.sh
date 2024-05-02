ddc9e86cd2f626bd8d42f6c94911208e86b9121d
Brian Campbell <86080337+campbeb3@users.noreply.github.com>
2024-05-02 15:55:46 -0700

    Update install.sh
    added Legacy 32-bit support and comments for each supported section

--- install.sh
+++ install.sh
@@ -16,29 +16,63 @@ mkdir -p /etc/stubby \
 # install cloudflared
 mkdir -p /tmp \
     && cd /tmp
+#####################################################
+###  ARM OS support  ################################
+#####################################################
 if [[ ${TARGETPLATFORM} =~ "arm64" ]]
 then
     curl -sL https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64.deb -o /tmp/cloudflared.deb
     dpkg --add-architecture arm64
     echo "$(date "+%d.%m.%Y %T") Added cloudflared for ${TARGETPLATFORM}" >> /build.info
+#####################################################
+###  Non-ARM OS support  ############################
+#####################################################
 elif [[ ${TARGETPLATFORM} =~ "amd64" ]]
 then 
     curl -sL https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb -o /tmp/cloudflared.deb
     dpkg --add-architecture amd64
     echo "$(date "+%d.%m.%Y %T") Added cloudflared for ${TARGETPLATFORM}" >> /build.info
+#####################################################
+###  Non-ARM 32-bit OS support  #####################
+#####################################################
 elif [[ ${TARGETPLATFORM} =~ "386" ]]
 then
     curl -sL https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-386.deb -o /tmp/cloudflared.deb
     dpkg --add-architecture 386
     echo "$(date "+%d.%m.%Y %T") Added cloudflared for ${TARGETPLATFORM}" >> /build.info
+#####################################################
+###  ARM 32-bit OS support  #########################
+#####################################################
 elif [[ ${TARGETPLATFORM} =~ 'arm/v7' ]]
 then
     curl -sL https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm.deb -o /tmp/cloudflared.deb
     dpkg --add-architecture arm
     echo "$(date "+%d.%m.%Y %T") Added cloudflared for ${TARGETPLATFORM}" >> /build.info
+#####################################################
+### Legacy 32-bit OS support  #######################
+#####################################################
+elif [[ ${TARGETPLATFORM} =~ 'arm/v6' ]]
+then
+    #curl -sL https://hobin.ca/cloudflared/latest?type=deb -o /tmp/cloudflared.deb
+    wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm
+else 
+    echo "$(date "+%d.%m.%Y %T") Unsupported platform - cloudflared not added" >> /build.info
+fi
+  if [[ ${TARGETPLATFORM} =~ 'arm/v6' ]]
+  then
+    sudo cp ./cloudflared-linux-arm /usr/local/bin/cloudflared
+    sudo chmod +x /usr/local/bin/cloudflared
+    cloudflared -v    
+  else
+    apt install /tmp/cloudflared.deb \
+        && rm -f /tmp/cloudflared.deb \
+        && useradd -s /usr/sbin/nologin -r -M cloudflared \
+        && chown cloudflared:cloudflared /usr/local/bin/cloudflared
+  fi 
 #elif [[ ${TARGETPLATFORM} =~ 'arm/v6' ]]
 #then
 #    curl -sL https://hobin.ca/cloudflared/releases/2022.3.1/cloudflared_2022.3.1_arm.deb -o /tmp/cloudflared.deb
+
 else 
     echo "$(date "+%d.%m.%Y %T") Unsupported platform - cloudflared not added" >> /build.info
 fi
